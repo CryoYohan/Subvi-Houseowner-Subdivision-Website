82ffefc88bdb375b1536416d18a6e4a96f0d1c87
xx441 <xx441@CCSLAB530U36>
2025-01-24 01:33:54 +0800

    Add project files.

---
+++ ELNET1-GROUP_PROJECT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ELNET1_GROUP_PROJECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
