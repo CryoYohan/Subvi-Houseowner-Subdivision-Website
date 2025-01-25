51c45e2399cc12b22c64f8d53ae09b62fec2c579
itsmecyril <cyrilypil@gmail.com>
2025-01-25 17:23:44 +0800

    test cshtml file

---
+++ ELNET1-GROUP_PROJECT/Controllers/HomeController1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ELNET1_GROUP_PROJECT.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Test  
+        public  IActionResult Test()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome(string name ="Unknown", int numtimes=1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}! Your age is {numtimes}");
+        }
+    }
+}
