3d9de3421af5d748045635cf97345178d128697e
unknown <XX441@ccs.local>
2025-02-04 09:14:45 +0800

    lab 530 local changes

---
+++ ELNET1-GROUP_PROJECT/Controllers/HomeController1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public IActionResult Index()
+    {
+        return View();
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome(string name = "Unknown", int ID = 1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    }
+    //
+    // GET: /admin/
+    [HttpGet]
+    public ContentResult Admin()
+    {
+        return new ContentResult
+        {
+            ContentType = "text/html",
+            Content = "<div>Hello World</div>"
+        };
+    }
+}
