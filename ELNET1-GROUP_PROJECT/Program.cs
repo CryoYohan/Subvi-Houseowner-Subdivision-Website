e648e0a3d01644cce913ecf4845f67fccdfc91ff
znarfieeee <watsszap23@gmail.com>
2025-02-08 17:00:06 +0800

    Added Controllers and cshtml for navigation

--- ELNET1-GROUP_PROJECT/Program.cs
+++ ELNET1-GROUP_PROJECT/Program.cs
@@ -38,6 +38,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=dashboard}/{id?}");
 
 app.Run();
